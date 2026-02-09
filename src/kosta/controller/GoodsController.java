2c1f3c6c58a477ea515b8a2a70500f22b1d7b0ec
LJG7123 <ljg7123@naver.com>
2026-02-09 17:05:18 +0900

    충돌 해결

--- src/kosta/controller/GoodsController.java
+++ src/kosta/controller/GoodsController.java
@@ -4,6 +4,7 @@ public class GoodsController {
     int i = 10;
     public void test() {
         System.out.println("원격에서 test추가 - 시나리오 5번 테스트");
+    	System.out.println("로컬에서 수정중인ㄷ");
     }
 
     public void selectAll() {
