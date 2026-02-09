51313017da85c33340ff87af195fdd04f921a131
LJG7123 <ljg7123@naver.com>
2026-02-09 16:04:33 +0900

    시나리오 3번 - GoodsService

--- src/kosta/service/GoodsService.java
+++ src/kosta/service/GoodsService.java
@@ -1,5 +1,12 @@
 package kosta.service;
 
 public class GoodsService {
-
+	public void insert() {
+		System.out.println("insert call");
+	}
+	
+	public void select() {
+		System.out.println("select 추가");
+		System.out.println("시나리오 3번 테스트");
+	}
 }
