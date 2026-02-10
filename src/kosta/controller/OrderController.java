211793184eaec5e0d55d3841c6919c954211000c
LJG7123 <ljg7123@naver.com>
2026-02-10 12:14:32 +0900

    OrderController 수정

--- src/kosta/controller/OrderController.java
+++ src/kosta/controller/OrderController.java
@@ -1,5 +1,11 @@
 package kosta.controller;
 
 public class OrderController {
-
+	public void orderInsert() {
+		System.out.println("order request");
+	}
+	
+	public void orderCart() {
+		System.out.println("cart 브랜치에서 추가");
+	}
 }
